8b77e3883ae4989eadd8c86ee78d45f79c7251e5
ZZZ <363201375@qq.com>
2022-09-28 10:19:55 +0800

    set header

--- main.go
+++ main.go
@@ -6,11 +6,12 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request)  {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
 	if r.URL.Path == "/" {
 		fmt.Fprint(w, "<h1>Hello, 这里是goblog!</h1>")
 	} else if r.URL.Path == "/about" {
 		fmt.Fprint(w,  "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
-			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+			"<a href=\"mailto:363201375@qq.com\">363201375@qq.com</a>")
 	} else {
 		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
 			"<p>如有疑惑，请联系我们。</p>")
