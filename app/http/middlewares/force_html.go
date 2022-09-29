5ca6cefd80e506f90ac99d40a5892351ed076ca8
ZZZ <363201375@qq.com>
2022-09-29 22:46:08 +0800

    重构中间件

---
+++ app/http/middlewares/force_html.go
@@ -0,0 +1,10 @@
+package middlewares
+
+import "net/http"
+
+func ForceHTMLMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-type", "text/html; charset=utf-8")
+		next.ServeHTTP(w, r)
+	})
+}
--- main.go
+++ main.go
@@ -1,72 +1,19 @@
 package main
 
 import (
-	"database/sql"
 	"github.com/gorilla/mux"
+	"goblog/app/http/middlewares"
 	"goblog/bootstrap"
-	"goblog/pkg/database"
 	"goblog/pkg/logger"
 	"net/http"
-	"strconv"
-	"strings"
 )
 
 var router *mux.Router
-var db *sql.DB
-
-type Article struct {
-	Title, Body string
-	ID          int64
-}
-
-func (article Article) Delete() (int64, error) {
-	rs, err := db.Exec("DELETE FROM articles WHERE id =" + strconv.FormatInt(article.ID, 10))
-	if err != nil {
-		return 0, err
-	}
-
-	if id, err := rs.RowsAffected(); id > 0 {
-		return id, err
-	}
-
-	return 0, nil
-}
-
-func (article Article) Link() string {
-	showURL, err := router.Get("articles.show").URL("id", strconv.FormatInt(article.ID, 10))
-	if err != nil {
-		logger.LogError(err)
-		return ""
-	}
-
-	return showURL.String()
-}
-
-func forceHTMLMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-type", "text/html; charset=utf-8")
-		next.ServeHTTP(w, r)
-	})
-}
-
-func removeTrailingSlash(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/" {
-			r.URL.Path = strings.TrimSuffix(r.URL.Path, "/")
-		}
-		next.ServeHTTP(w, r)
-	})
-}
 
 func main() {
-	database.Initialize()
-	db = database.DB
-
 	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
 
-	router.Use(forceHTMLMiddleware)
-
-
-	http.ListenAndServe(":3000", removeTrailingSlash(router))
+	err := http.ListenAndServe(":3000", middlewares.RemoveTrailingSlash(router))
+	logger.LogError(err)
 }
