ff27880b33a2b1e41eabfb416e4bc714bd3e99b1
ZZZ <363201375@qq.com>
2022-09-29 11:33:55 +0800

    删除文章

--- main.go
+++ main.go
@@ -29,6 +29,19 @@ type Article struct {
 	ID          int64
 }
 
+func (article Article) Delete() (int64, error) {
+	rs, err := db.Exec("DELETE FROM articles WHERE id =" + strconv.FormatInt(article.ID, 10))
+	if err != nil {
+		return 0, err
+	}
+
+	if id, err := rs.RowsAffected(); id > 0 {
+		return id, err
+	}
+
+	return 0, nil
+}
+
 func (article Article) Link() string {
 	showURL, err := router.Get("articles.show").URL("id", strconv.FormatInt(article.ID, 10))
 	if err != nil {
@@ -44,7 +57,7 @@ func initDB()  {
 	config := mysql.Config{
 		User: "root",
 		Passwd: "root",
-		Addr: "192.168.0.102:3307",
+		Addr: "192.168.0.104:3307",
 		Net: "tcp",
 		DBName: "goblog",
 		AllowNativePasswords: true,
@@ -83,6 +96,21 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request)  {
 			"<p>如有疑惑，请联系我们。</p>")
 }
 
+func RouteName2URL(routeName string, pairs ...string) string {
+	url, err := router.Get(routeName).URL(pairs...)
+
+	if err != nil {
+		checkError(err)
+		return ""
+	}
+
+	return url.String()
+}
+
+func Int64ToString(num int64) string {
+	return strconv.FormatInt(num, 10)
+}
+
 func articlesShowHandler(w http.ResponseWriter, r *http.Request)  {
 	id := getRouteVariable("id", r)
 
@@ -98,7 +126,10 @@ func articlesShowHandler(w http.ResponseWriter, r *http.Request)  {
 			fmt.Fprint(w, "服务器内部错误")
 		}
 	} else {
-		tmpl, err := template.ParseFiles("resources/views/articles/show.gohtml")
+		tmpl, err := template.New("show.gohtml").Funcs(template.FuncMap{
+			"RouteName2URL": RouteName2URL,
+			"Int64ToString": Int64ToString,
+		}).ParseFiles("resources/views/articles/show.gohtml")
 		checkError(err)
 
 		err = tmpl.Execute(w, article)
@@ -281,6 +312,39 @@ func articlesStoreHandler(w http.ResponseWriter, r *http.Request)  {
 	}
 }
 
+func articlesDeleteHandler(w http.ResponseWriter, r *http.Request)  {
+	id := getRouteVariable("id", r)
+
+	article, err := getArticleByID(id)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			w.WriteHeader(http.StatusNotFound)
+			fmt.Fprint(w, "404 文章未找到")
+		} else {
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		}
+	} else {
+		rowsAffected, err := article.Delete()
+
+		if err != nil {
+			checkError(err)
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "500 服务器内部错误")
+		} else {
+			if rowsAffected > 0 {
+				indexURL, _ := router.Get("articles.index").URL()
+				http.Redirect(w, r, indexURL.String(), http.StatusFound)
+			} else {
+				w.WriteHeader(http.StatusNotFound)
+				fmt.Fprint(w, "404 文章未找到")
+			}
+		}
+	}
+}
+
 func saveArticleToDB(title string, body string) (int64, error) {
 	var (
 		id int64
@@ -368,6 +432,7 @@ func main() {
 	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("articles.create")
 	router.HandleFunc("/articles/{id:[0-9]+}/edit", articlesEditHandler).Methods("GET").Name("articles.edit")
 	router.HandleFunc("/articles/{id:[0-9]+}", articlesUpdateHandler).Methods("POST").Name("articles.update")
+	router.HandleFunc("/articles/{id:[0-9]+}/delete", articlesDeleteHandler).Methods("POST").Name("articles.delete")
 
 	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
 
